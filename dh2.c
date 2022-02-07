17c26ab3d53d719140fc83e7a3e316385e0d9ae6
Christopher Snowhill <kode54@gmail.com>
2022-02-06 21:01:40 -0800

    Rewrote dh processor to work with new convolver
    Rewrote the sampler code to operate in blocks, which greatly speeds
    things up, versus working with one sample at a time. Also reformatted
    according to clang formatting style.

--- dh2.c
+++ dh2.c
@@ -1,29 +1,25 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #include "simple_convolver.h"
 
 #include "samples.h"
 
-unsigned int get_le16(const unsigned char * ptr)
-{
+unsigned int get_le16(const unsigned char *ptr) {
 	return ptr[0] + (ptr[1] << 8);
 }
 
-unsigned int get_le32(const unsigned char * ptr)
-{
+unsigned int get_le32(const unsigned char *ptr) {
 	return ptr[0] + (ptr[1] << 8) + (ptr[2] << 16) + (ptr[3] << 24);
 }
 
-unsigned int get_be32(const unsigned char * ptr)
-{
+unsigned int get_be32(const unsigned char *ptr) {
 	return ptr[3] + (ptr[2] << 8) + (ptr[1] << 16) + (ptr[0] << 24);
 }
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv) {
 	FILE *f, *g;
 	unsigned char buffer[1024];
 	unsigned int riff_size, fmt_size, data_offset, data_size;
@@ -32,44 +28,40 @@ int main(int argc, char ** argv)
 	unsigned int sample, preset, samples_written;
 	unsigned int i;
 
-	const speaker_preset * set;
+	const speaker_preset *set;
 
 	void *conv;
 
-	float samples[6];
+	float inbuffer[1024 * 6];
+	float outbuffer[1024 * 2];
 
-	if (argc != 3)
-	{
+	if(argc != 3) {
 		fprintf(stderr, "Usage:\tdh2 <input.wav> <output.raw>\n");
 		return 1;
 	}
 
 	f = fopen(argv[1], "rb");
 
-	if ( !f )
-	{
+	if(!f) {
 		fprintf(stderr, "Unable to open %s.\n", argv[1]);
 		return 1;
 	}
 
-	if ( fread(buffer, 1, 8, f) != 8 )
-	{
+	if(fread(buffer, 1, 8, f) != 8) {
 		fclose(f);
 		fprintf(stderr, "Unable to read WAV header.\n");
 		return 1;
 	}
 
-	if ( get_be32( buffer ) != 'RIFF' )
-	{
+	if(get_be32(buffer) != 'RIFF') {
 		fclose(f);
 		fprintf(stderr, "Not a RIFF file.\n");
 		return 1;
 	}
 
-	riff_size = get_le32( buffer + 4 );
+	riff_size = get_le32(buffer + 4);
 
-	if ( riff_size < 4 )
-	{
+	if(riff_size < 4) {
 		fclose(f);
 		fprintf(stderr, "RIFF too small.\n");
 		return 1;
@@ -78,8 +70,7 @@ int main(int argc, char ** argv)
 	fread(buffer, 1, 4, f);
 	riff_size -= 4;
 
-	if ( get_be32( buffer ) != 'WAVE' )
-	{
+	if(get_be32(buffer) != 'WAVE') {
 		fclose(f);
 		fprintf(stderr, "Not WAVE format.\n");
 		return 1;
@@ -88,69 +79,57 @@ int main(int argc, char ** argv)
 	fmt_size = 0;
 	data_size = 0;
 
-	while ( riff_size >= 8 )
-	{
+	while(riff_size >= 8) {
 		fread(buffer, 1, 8, f);
 		riff_size -= 8;
 
 		id = get_be32(buffer);
-		if (id == 'fmt ')
-		{
-			if (fmt_size)
-			{
+		if(id == 'fmt ') {
+			if(fmt_size) {
 				fclose(f);
 				fprintf(stderr, "Multiple fmt chunks found.\n");
 				return 1;
 			}
 
 			fmt_size = get_le32(buffer + 4);
-			if (fmt_size & 1) ++fmt_size;
+			if(fmt_size & 1) ++fmt_size;
 			fread(buffer, 1, fmt_size, f);
 			riff_size -= fmt_size;
 
 			sample_rate = get_le32(buffer + 4);
-		}
-		else if (id == 'data')
-		{
-			if (data_size)
-			{
+		} else if(id == 'data') {
+			if(data_size) {
 				fclose(f);
 				fprintf(stderr, "Multiple data chunks found.\n");
 				return 1;
 			}
 
 			data_size = get_le32(buffer + 4);
-			if (data_size & 1) data_size++;
+			if(data_size & 1) data_size++;
 			riff_size -= data_size;
 			data_offset = ftell(f);
 			fseek(f, data_size, SEEK_CUR);
-		}
-		else
-		{
+		} else {
 			int size = get_le32(buffer + 4);
-			if (size & 1) size++;
+			if(size & 1) size++;
 			riff_size -= size;
 			fseek(f, size, SEEK_CUR);
 		}
 	}
 
-	if (!fmt_size || !data_size)
-	{
+	if(!fmt_size || !data_size) {
 		fclose(f);
-		if (!fmt_size)
-		{
+		if(!fmt_size) {
 			fprintf(stderr, "Missing fmt chunk.\n");
 		}
-		if (!data_size)
-		{
+		if(!data_size) {
 			fprintf(stderr, "Missing data chunk.\n");
 		}
 		return 1;
 	}
 
 	g = fopen(argv[2], "wb");
-	if (!g)
-	{
+	if(!g) {
 		fclose(f);
 		fprintf(stderr, "Unable to open %s for writing.\n", argv[2]);
 		return 1;
@@ -162,9 +141,8 @@ int main(int argc, char ** argv)
 
 	set = speaker_presets[1];
 
-	for (preset = 0; preset < speaker_preset_count; ++preset)
-	{
-		if (set[preset].frequency == sample_rate) break;
+	for(preset = 0; preset < speaker_preset_count; ++preset) {
+		if(set[preset].frequency == sample_rate) break;
 	}
 
 	conv = convolver_create(set[preset].impulses->impulse, set[preset].impulses->count, 6, 2, 2);
@@ -172,34 +150,22 @@ int main(int argc, char ** argv)
 	sample = 0;
 	samples_written = 0;
 
-	while (samples_written < sample_count)
-	{
-		unsigned int samples_free;
-		unsigned int samples_ready = convolver_ready(conv);
-		while (samples_ready)
-		{
-			convolver_read(conv, samples);
-			fwrite(samples, 4, 2, g);
-			++samples_written;
-			--samples_ready;
-			if (samples_written == sample_count) break;
+	while(samples_written < sample_count) {
+		size_t samples_in = 0;
+		while(samples_in < 1024 && sample < sample_count) {
+			size_t samples_to_read = sample_count - sample;
+			if(samples_to_read > 1024 - samples_in)
+				samples_to_read = 1024 - samples_in;
+			fread(inbuffer + samples_in * 6, 4 * 6, samples_to_read, f);
+			sample += samples_to_read;
+			samples_in += samples_to_read;
 		}
-		if (samples_written == sample_count) break;
-		samples_free = convolver_get_free_count(conv);
-		while (samples_free)
-		{
-			if (sample < sample_count)
-			{
-				fread(samples, 4, 6, f);
-				++sample;
-			}
-			else
-			{
-				memset(samples, 0, 6*4);
-			}
-			convolver_write(conv, samples);
-			--samples_free;
-		}
+
+		convolver_run(conv, inbuffer, outbuffer, samples_in);
+
+		fwrite(outbuffer, 2 * 4, samples_in, g);
+
+		samples_written += samples_in;
 	}
 
 	fclose(g);
